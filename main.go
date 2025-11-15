2cc813e40b9894cf1ce00f64fabf91ae3be6a725
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-11-15 20:13:48 +0100

    Initial implementation

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path"
+
+	"github.com/go-git/go-git/v6"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: gcl https://example.com/user/repo")
+		os.Exit(1)
+	}
+	gitUrl := os.Args[1]
+
+	urlComponents, err := url.Parse(gitUrl)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error %w", err))
+	}
+
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error %w", err))
+	}
+
+	clonePath := path.Join(homedir, "code", urlComponents.Host, urlComponents.Path)
+	fmt.Printf("Clone Path: %s\n", clonePath)
+
+	err = os.MkdirAll(clonePath, 0750)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error %w", err))
+	}
+
+	_, err = git.PlainClone(clonePath, &git.CloneOptions{
+		URL:               gitUrl,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		Progress:          os.Stdout,
+	})
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error %w", err))
+	}
+
+}
