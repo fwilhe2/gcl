8fcfd0cdd6005de09a296e85ea76b77679a411ee
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2025-11-17 18:49:34 +0100

    test

--- main.go
+++ main.go
@@ -2,15 +2,7 @@ package main
 
 import "github.com/fwilhe2/gcl/cmd"
 
-var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
-)
 
 func main() {
-	cmd.Version = version
-	cmd.Commit = commit
-	cmd.Date = date
 	cmd.Execute()
 }
